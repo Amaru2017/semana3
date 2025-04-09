5b9a8768c505311cc33d0d8c5b784e4637e1c2c6
Johan <johanramos180@gmail.com>
2025-04-09 10:25:54 -0500

    cambios desde visual studio

--- CLASE1/Program.cs
+++ CLASE1/Program.cs
@@ -13,6 +13,7 @@ namespace CLASE1
             Console.Write("Bienvenidos al curso de C#");
             Console.Write("Bienvenidos al curso de C#");
             Console.Write("Bienvenidos al curso de C#");
+            Console.Write("Bienvenidos al curso de C#");
             Console.ReadKey();
 
         }
